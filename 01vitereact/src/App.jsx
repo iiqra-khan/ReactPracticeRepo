49d013a3046d5df74d4a692ab8639c76773acbf4
Iqra Khan <iqrakhan89102@gmail.com>
2024-12-22 21:01:29 +0530

    initialize React and Vite project with basic components and configuration

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+import Test from "./test"
+
+function App() {
+
+  return (
+    <> 
+    <Test/>
+     <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <h4>this is a test function</h4>
+    )
+}
+
+export default Test
